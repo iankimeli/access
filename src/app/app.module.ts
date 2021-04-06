e0f4bcc4a67d3a52942a960fa5da8d2b1aa21c16
Ian <iankimeli@gmail.com>
2021-04-06 13:32:48 +0300

    added repositories class

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavigationComponent } from './navigation/navigation.component';
+import { RepositoriesComponent } from './repositories/repositories.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavigationComponent
+    NavigationComponent,
+    RepositoriesComponent
   ],
   imports: [
     BrowserModule,
